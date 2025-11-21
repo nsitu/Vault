be1014cfbc7888b94198a29f1b9eaacb04de63a9
Harold Sikkema <harold@nsitu.ca>
2025-11-21 15:28:29 -0500

    Refactor HTML structure and enhance JavaScript functionality for landing and vault pages; update styles for improved aesthetics and usability

--- public/script.js
+++ public/script.js
@@ -1,87 +1,60 @@
-const userWelcome = async () => {
-    // the profile endpoint will give us information 
-    // about the currently logged in user. 
-    const response = await fetch('/api/user')
-    const data = await response.json()
+const isLandingPage = document.getElementById('landing') ? true : false
+const isVaultPage = document.getElementById('vault') ? true : false
 
-    // if the user is logged in,
-    // we can personalize the page for them
-    if (data.isAuthenticated) {
-        const isLanding = document.getElementById('landing') !== null
-        const vaultLink = isLanding
-            ? '<a class="button" href="/vault">Enter Vault</a>'
-            : ''
+const response = await fetch('/api/user')
+const data = await response.json()
 
-        const safePicture = data.picture || 'user.svg'
+const isAuthenticated = data.isAuthenticated
+const displayName = data.name || 'Guest'
+const displayEmail = data.email || 'Welcome'
+const profilePicture = data.picture || 'user.svg'
 
-        profile.innerHTML = `
-        <img src="${safePicture}" onerror="this.onerror=null;this.src='user.svg';">
+const authButton = isAuthenticated
+    ? '<a class="button" href="/logout">Logout</a>'
+    : '<a class="button" href="/login">Login</a>'
+
+const vaultLink = isAuthenticated && isLandingPage
+    ? '<a class="button" href="/vault">Enter Vault</a>'
+    : ''
+
+profile.innerHTML = `
+        <img src="${profilePicture}" onerror="this.onerror=null;this.src='user.svg';">
         <div>
-            <h4>${data.name}</h4>
-            <h5>${data.email}</h5>
+            <h4>${displayName}</h4>
+            <h5>${displayEmail}</h5>
         </div>
-        <a class="button" href="/logout">Logout</a>
+        ${authButton}
         ${vaultLink}
-        `
-        if (isLanding) {
-            content.innerHTML = `<span class="secret-sauce">
-            Your <a href="/vault">secret sauce</a> is ready.
-            </span>`;
-        }
-        // Only load vault data on pages that show content
-        if (document.getElementById('vault')) {
-            displayVault()
-        }
-    }
-    // If the user is not logged in 
-    // we should show a Guest Landing page with a Login button
-    else {
-        profile.innerHTML = ` 
-        <img src="user.svg">
-        <div>
-            <h4>Guest</h4>
-            <h5>Welcome</h5> 
-        </div>
-        <a class="button" href="/login">Login</a>
-        `
-        if (document.getElementById('landing')) {
-            content.innerHTML = `<span class="secret-sauce">
-            Have you tried our <a href="/vault">secret sauce</a>?
-            </span>`;
-        }
-    }
-    if (logo) {
-        logo.addEventListener('click', () => {
-            window.location.href = '/'
-        })
-    }
+    `
+
+if (isLandingPage && content) {
+    content.innerHTML = isAuthenticated
+        ? `<span class="secret-sauce">
+                    Your <a href="/vault">secret sauce</a> is ready.
+               </span>`
+        : `<span class="secret-sauce">
+                    Have you tried our <a href="/vault">secret sauce</a>?
+               </span>`
 }
 
-
-
-userWelcome()
-
-// template for each secret item
-const template = (item) =>
-    `<li>
-        <a class="sauce" target="_blank" href="${item.url}">
-            <span>${item.name}</span>
-        </a>
-    </li>`
-
-
-const displayVault = async () => {
+if (isAuthenticated && isVaultPage) {
     // the vault endpoint only works for logged in users
     // see also, the /api/secrets endpoint in ./server.js
     try {
         const response = await fetch('/api/secrets')
         const data = await response.json()
-
-        const listItems = data.map(item => template(item)).join('')
-
-        vaultHTML = `<span class="secret-sauce">
+        const vaultHTML = `<span class="secret-sauce">
             Here you go.
-            </span><ul class="items">${listItems}</ul>`;
+            </span>
+            <ul class="items">
+                ${data.map(item =>
+            `<li>
+                    <a class="sauce" target="_blank" href="${item.url}">
+                        <span>${item.name}</span>
+                    </a>
+                </li>`)
+                .join('')}
+            </ul>`;
 
         content.innerHTML = DOMPurify.sanitize(vaultHTML)
     }
@@ -90,3 +63,8 @@ const displayVault = async () => {
     }
 }
 
+if (logo) {
+    logo.addEventListener('click', () => {
+        window.location.href = '/'
+    })
+}
